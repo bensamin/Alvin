8d53f96b07043d6e5d140f10e85dfe41d868531d
WEB <WEB@localhost>
2017-03-31 14:10:15 +0800

    问卷

---
+++ web/test.js
@@ -0,0 +1,13 @@
+ {
+ "id":"1",
+ "name":"Alvin",
+ "dscr":"just test",
+ "piscId":"kitty",
+ "NumEasy":"10",
+ "ScoreEasy":"5",
+ "NumJust":"5",
+ "ScoreJust":"10",
+ "NumTrouble":"2",
+ "ScoreTrouble":"20" 
+	} 
+
